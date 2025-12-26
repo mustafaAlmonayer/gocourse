bf265696f579d2e12cbf28b3d53093a570590176
mustafa <mustafa.almonayer@gmail.com>
2025-12-26 14:41:46 +0300

    init commit

---
+++ basics/naming_conventions.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type Employee struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func main() {
+
+	// Pascal Case
+	// Eg. CalculateArea, UserProfile, OrderDetails
+	// Structs, Interfaces, Enums
+
+	// snake case
+	// Eg. user_id, first_name, http_request
+	// files
+
+	//UPERCASE
+	// Use case is for Constants
+	// Eg. MAX_CONNECTIONS, DEFAULT_TIMEOUT
+
+	// Mixed Case
+	// Eg. isValid, getUserName
+	// Eg. Variables, Functions
+
+	const MAX_CONNECTIONS = 100
+
+	var employeeID = 12345
+	fmt.Println("Employee ID:", employeeID)
+}
---
+++ basics/variables.go
@@ -0,0 +1,33 @@
+package main
+
+// only accessible within package main
+import "fmt"
+
+var middleName = "Cane"
+
+func main() {
+	// var age int
+	// var name string = "John"
+	// var name1 = "Jane"
+
+	// count := 10
+	// lastName := "Smith"
+	// middleName := "Cane"
+
+	// Default values
+	// Numeric types default to 0
+	// String type defaults to ""
+	// Boolean type defaults to false
+	// Pointers, slices, maps, functions, structs, channels, and interfaces default to nil
+
+	// ---- Scope
+	middleName := "Alexander"
+	fmt.Println(middleName) // This will cause an error because firstName is not in scope here
+	printName()
+
+}
+
+func printName() {
+	firstName := "Michael"
+	fmt.Println(firstName, middleName)
+}
